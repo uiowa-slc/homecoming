b1080639ad6dfe78be189c6d84165d649ae754e8
Brandon Ewoldt <bewoldt1@gmail.com>
2014-01-08 13:28:06 -0600

    adding image and location fields
    Signed-off-by: Brandon Ewoldt <bewoldt1@gmail.com>

--- mysite/code/CalendarEventExtension.php
+++ mysite/code/CalendarEventExtension.php
@@ -2,18 +2,23 @@
 class CalendarEventExtension extends DataExtension {
 
 	private static $db = array(
-
+    'EventLocation' => 'Text',
 	);
 
 	private static $has_one = array(
+    "EventImage" => "Image"
 	);
 
 	private static $many_many = array (
 		"Categories" => "Category"
 	);
    public function updateCMSFields(FieldList $fields) {
-   		$tagField = new TagField('Categories', 'Categories (separated by spaces, no commas)', null, 'CalendarEvent');		
-        $fields->addFieldToTab("Root.Main", $tagField, "Content");
+
+   		$tagField = new TagField('Categories', 'Categories (separated by spaces, no commas)', null, 'CalendarEvent');
+      $fields->addFieldToTab("Root.Main", new TextField("EventLocation", "Location"));
+      $fields->addFieldToTab("Root.Main", new UploadField("EventImage", "Image"));
+      $fields->addFieldToTab("Root.Main", $tagField, "Content");
+
     }
 	/*public function getCMSFields(){
 		$fields = parent::getCMSFields();
