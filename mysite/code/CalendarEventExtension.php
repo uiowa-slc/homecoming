f1865cdd69bd654ec6deb92fe1fdda9dee3627dd
Dustin Quam <quamsta@gmail.com>
2014-01-24 10:47:22 -0600

    replaced tag field

--- mysite/code/CalendarEventExtension.php
+++ mysite/code/CalendarEventExtension.php
@@ -14,9 +14,25 @@ class CalendarEventExtension extends DataExtension {
 	);
    public function updateCMSFields(FieldList $fields) {
 
-   	  $tagField = new TagField('Categories', 'Categories: students faculty-staff alumni-friends (separated by spaces, no commas)', null, 'CalendarEvent');
+   	  //$tagField = new TagField('Categories', 'Categories: students faculty-staff alumni-friends (separated by spaces, no commas)', null, 'CalendarEvent');
       //$tagField = new TreeMultiselectField('Categories', 'Categories', "Category", "ID", "Title" );
 
+	$categoriesMap = array();
+	foreach(Category::get() as $category) {
+	// Listboxfield values are escaped, use ASCII char instead of &raquo;
+	$categoriesMap[$category->ID] = $category->Title;
+
+	}
+
+	asort($categoriesMap);
+	
+	$tagField = ListboxField::create('Categories', 'Categories')
+		->setMultiple(true)
+		->setSource($categoriesMap)
+		->setAttribute(
+			'data-placeholder', 
+			'Add Tags'
+		);
 
       $fields->addFieldToTab("Root.Main", new TextField("EventLocation", "Location"));
       $fields->addFieldToTab("Root.Main", new UploadField("EventImage", "Image"));
