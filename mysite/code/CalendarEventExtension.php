148454caee7edc6ddf1ac8d873261fffd3a3fec4
Dustin Quam <quamsta@gmail.com>
2014-02-12 12:02:41 -0600

    comething with the calendar event extension added divsion bar to composer

--- mysite/code/CalendarEventExtension.php
+++ mysite/code/CalendarEventExtension.php
@@ -2,53 +2,39 @@
 class CalendarEventExtension extends DataExtension {
 
 	private static $db = array(
-    'EventLocation' => 'Text',
+	'EventLocation' => 'Text',
 	);
 
 	private static $has_one = array(
-    "EventImage" => "Image"
+	"EventImage" => "Image"
 	);
 
 	private static $many_many = array (
 		"Categories" => "Category"
 	);
-   public function updateCMSFields(FieldList $fields) {
 
-   	  //$tagField = new TagField('Categories', 'Categories: students faculty-staff alumni-friends (separated by spaces, no commas)', null, 'CalendarEvent');
-      //$tagField = new TreeMultiselectField('Categories', 'Categories', "Category", "ID", "Title" );
+	public function updateCMSFields(FieldList $fields) {
 
-	$categoriesMap = array();
-	foreach(Category::get() as $category) {
-	// Listboxfield values are escaped, use ASCII char instead of &raquo;
-	$categoriesMap[$category->ID] = $category->Title;
+		$categoriesMap = array();
+		foreach(Category::get() as $category) {
+			$categoriesMap[$category->ID] = $category->Title;
+		}
+
+		asort($categoriesMap);
+		
+		$tagField = ListboxField::create('Categories', 'Categories')
+			->setMultiple(true)
+			->setSource($categoriesMap)
+			->setAttribute(
+				'data-placeholder', 
+				'Add Tags'
+			);
+
+		  $fields->addFieldToTab("Root.Main", new TextField("EventLocation", "Location"));
+		  $fields->addFieldToTab("Root.Main", new UploadField("EventImage", "Image"));
+		  $fields->addFieldToTab("Root.Main", $tagField, "Content");
 
 	}
 
-	asort($categoriesMap);
-	
-	$tagField = ListboxField::create('Categories', 'Categories')
-		->setMultiple(true)
-		->setSource($categoriesMap)
-		->setAttribute(
-			'data-placeholder', 
-			'Add Tags'
-		);
-
-      $fields->addFieldToTab("Root.Main", new TextField("EventLocation", "Location"));
-      $fields->addFieldToTab("Root.Main", new UploadField("EventImage", "Image"));
-      $fields->addFieldToTab("Root.Main", $tagField, "Content");
-
-    }
-
-
-	/*public function getCMSFields(){
-		$fields = parent::getCMSFields();
-
-		
-		
-		return $fields;
-		
-	}*/
-	
 
 }
