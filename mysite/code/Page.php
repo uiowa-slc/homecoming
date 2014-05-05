25e7ef0ae52b6a94040c08aabc0538586265cbfb
Brandon Ewoldt <bewoldt1@gmail.com>
2014-05-05 15:09:23 -0500

    removing division bar injection
    Signed-off-by: Brandon Ewoldt <bewoldt1@gmail.com>

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -42,6 +42,7 @@ class Page_Controller extends ContentController {
 
 	public function init() {
 		parent::init();
+		Requirements::block('division-bar/css/_division-bar.css');
 		// You can include any CSS or JS required by your project here.
 		// See: http://doc.silverstripe.org/framework/en/reference/requirements
 	}
