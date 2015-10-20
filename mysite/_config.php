bf551f52464c73b0ff891d5c3158529a227fa51d
Dustin Quam <dustinquam@me.com>
2015-10-20 14:36:19 -0500

    force ssl

--- mysite/_config.php
+++ mysite/_config.php
@@ -5,17 +5,17 @@ $project = 'mysite';
 
 global $database;
 $database = 'homecoming';
- 
+
 // Use _ss_environment.php file for configuration
-require_once("conf/ConfigureFromEnv.php");
+require_once ("conf/ConfigureFromEnv.php");
 
 Calendar::add_extension('CalendarExtension');
 CalendarEvent::add_extension('CalendarEventExtension');
 
+Director::forceSSL();
 
 // Set the site locale
 i18n::set_locale('en_US');
-
 if(Director::isLive()) {
 	Director::forceSSL(array('/^Security/','/^admin/'));
 }
